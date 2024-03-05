42df207b3a18f22c9db02838449953e143bccc22
AdamLeijman <adam.leijman@hotmail.com>
2024-03-05 11:48:20 +0100

    Update HighScore.java

--- MazeGen/program/src/main/java/com/example/program/model/HighScore.java
+++ MazeGen/program/src/main/java/com/example/program/model/HighScore.java
@@ -15,7 +15,7 @@ public class HighScore {
     private final String[] list = new String[10];
     private int place = 1;
     //private final String path = "/com/example/program/files/HighScoreList.txt";
-    private final String path = "/Users/adamleijman/Kurser/År2/DA489A_Systemutveckling2/MazeGen/MazeGen/program/src/main/resources/com/example/program/files/HighScoreList.txt";
+    private final String path = "/Users/adamleijman/Desktop/MazeGen/MazeGen/program/src/main/resources/com/example/program/files/HighScoreList.txt";
 
     /**
      * Inputs a new person and time to the highScoreList.
